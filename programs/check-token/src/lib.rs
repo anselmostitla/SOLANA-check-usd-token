4f601fb1ef929ed36fb5ace73f999af4864415e5
anselmo <anselmostitla@gmail.com>
2025-01-24 12:59:58 -0600

    Finishing initialize_data_account, DataAccount and beginning enum BankrunError.

--- programs/check-token/src/lib.rs
+++ programs/check-token/src/lib.rs
@@ -10,11 +10,17 @@ const TOKEN_MINIMUM_BALANCE:u64 = 100_000_000_000;
 const USDC_MINT:&str = "EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v";
 
 #[program]
-pub mod check_token {
+pub mod check_usdc_token {
     use super::*;
 
     pub fn initialize_data_account(ctx: Context<InitializedDataAccount>) -> Result<()> {
-        Ok(())
+        let current_slot = Clock::get()?.slot;
+        msg!("Current slot: {}", current_slot);
+        require_gte!(current_slot, MINIMUM_SLOT, BankrunError::InvalidSlot);
+        ctx.accounts.data_account.pub_key = ctx.accounts.some_user_account.key(); // public key that uniquely identifies the account "new_data"
+        ctx.accounts.data_account.last_updated_account = current_slot;
+        msg!("Set data_account pub_key: {}", ctx.accounts.some_user_account.key());
+        Ok(()) 
     }
 }
 
@@ -33,4 +39,16 @@ pub struct InitializedDataAccount<'info> {
     pub some_user_account: Signer<'info>,
 
     system_program: Program<'info, System>,
+}
+
+#[account]
+#[derive(InitSpace)]
+pub struct DataAccount {
+    pub last_updated_account: u64,
+    pub pub_key: Pubkey,
+}
+
+#[error_code]
+pub enum BankrunError {
+    InvalidSlot, 
 }
